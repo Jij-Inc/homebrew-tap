1ab4f21bb393048a83f93ca1e70563c738eb09f5
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-08-14 21:05:38 +0900

    Add libpapilo formula

---
+++ Formula/libpapilo.rb
@@ -0,0 +1,24 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Libpapilo < Formula
+  desc "libpapilo: PaPILO as a Presolve-Only Shared Library with C API"
+  homepage "https://github.com/Jij-Inc/libpapilo"
+  url "https://github.com/Jij-Inc/libpapilo/releases/download/v2.4.3-jij.1/libpapilo-2.4.3-jij.1-Darwin-libpapilo.tar.gz"
+  sha256 "68aad83e0f22b446c98a39362a3253e8fe48206a350bbe0e259a95239a07e2f0"
+  license "LGPL-3.0"
+
+  def install
+    include.install "include/libpapilo.h"
+    lib.install Dir["lib/*"]
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include "libpapilo.h"
+      int main() { return 0; }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-lpapilo", "-o", "test"
+    system "./test"
+  end
+end
