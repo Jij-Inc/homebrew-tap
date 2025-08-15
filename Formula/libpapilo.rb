70551a16106642267abbb16d69242b824650f6ca
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-08-15 14:04:58 +0900

    Explicit versioning "2.4.3-jij.1" of the libpapilo formula

--- Formula/libpapilo.rb
+++ Formula/libpapilo.rb
@@ -2,6 +2,7 @@ class Libpapilo < Formula
   desc "PaPILO as a Presolve-Only Shared Library with C API"
   homepage "https://github.com/Jij-Inc/libpapilo"
   url "https://github.com/Jij-Inc/libpapilo/releases/download/v2.4.3-jij.1/libpapilo-2.4.3-jij.1-Darwin-libpapilo.tar.gz"
+  version "2.4.3-jij.1"
   sha256 "68aad83e0f22b446c98a39362a3253e8fe48206a350bbe0e259a95239a07e2f0"
   license "LGPL-3.0"
 
