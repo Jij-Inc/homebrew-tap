f22ce0624bc280fa83e512e30a191495319211c5
Toshiki Teramura <toshiki.teramura@gmail.com>
2026-03-10 17:51:34 +0900

    libpapilo 3.0.0-jij.2 (#7)
    Created with `brew bump-formula-pr`.

--- Formula/libpapilo.rb
+++ Formula/libpapilo.rb
@@ -1,9 +1,9 @@
 class Libpapilo < Formula
   desc "PaPILO as a Presolve-Only Shared Library with C API"
   homepage "https://github.com/Jij-Inc/libpapilo"
-  url "https://github.com/Jij-Inc/libpapilo/releases/download/v3.0.0-jij.1/libpapilo-3.0.0-jij.1-Darwin-libpapilo.tar.gz"
-  version "3.0.0-jij.1"
-  sha256 "83f14d1c2871b0194747d948ef8977d2d2bad599b413e536bf7fecf7baf718f7"
+  url "https://github.com/Jij-Inc/libpapilo/releases/download/v3.0.0-jij.2/libpapilo-3.0.0-jij.2-Darwin-libpapilo.tar.gz"
+  version "3.0.0-jij.2"
+  sha256 "e4df09540826550b2a845bae86fb30063dc96365153ffb922473eecf29236df0"
   license "Apache-2.0"
 
   livecheck do
